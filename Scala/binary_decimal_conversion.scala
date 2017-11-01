c096210cebaabe7b7a62d579c32f8bde382b05ca
svadwlas <webdeveloper.vadwlas@gmail.com>
2017-11-01 18:00:48 -0400

    Update binary_decimal_conversion.scala

--- Scala/binary_decimal_conversion.scala
+++ Scala/binary_decimal_conversion.scala
@@ -11,3 +11,29 @@ def main(args: Array[String]): Unit = {
     println(num.toBinaryString)
 
 }
+
+/* Use the good stuff in Scala! Pattern Matching - The whole idea of being Functional!!! Brevity
+   You can use tail recursion to optimize this further. */
+
+
+  def convertDecimalToBinary(dec: Int): String = dec match {
+
+    case 0|1 => s"$dec"
+    case _ => s"${convertDecimalToBinary(dec/2)}${dec%2}"
+
+
+  }
+
+/* In Scala you can easily convert from hexadecimal to binary and vice-versa. If needed a string interpolator or
+an "implicit class" can be used, for all conversions like Octal etc."
+*/
+
+
+  def convertBinaryToDecimal(bin: String): Int ={
+
+    val x = Integer.parseInt(bin,2)
+    return x
+
+
+  }
+
