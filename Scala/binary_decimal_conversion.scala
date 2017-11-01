1b05464b3c08a44a58153ddec721055f8e46fc46
svadwlas <webdeveloper.vadwlas@gmail.com>
2017-11-01 17:15:20 -0400

    Update binary_decimal_conversion.scala

--- Scala/binary_decimal_conversion.scala
+++ Scala/binary_decimal_conversion.scala
@@ -1,1 +1,13 @@
 
+/*
+This Scala program converts from a Binary Number to a Decimal as well as from a Decimal to a Binary Number.
+*/
+
+
+// There is a built in function in Scala, in std lib
+
+def main(args: Array[String]): Unit = {
+   val num = 42
+    println(num.toBinaryString)
+
+}
