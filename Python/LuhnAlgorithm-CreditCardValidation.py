5c1bb3fbb0d676cec39ebd4137b43c1b030531b8
svadwlas <webdeveloper.vadwlas@gmail.com>
2018-03-12 13:15:15 -0400

    Update LuhnAlgorithm-CreditCardValidation.py

--- Python/LuhnAlgorithm-CreditCardValidation.py
+++ Python/LuhnAlgorithm-CreditCardValidation.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     number = input('Enter the credit card number').replace(' ', '')
     if not number.isdigit():
-        raise Exception('Invalid Credit Card number. Make sure its all digits with optional spaces')
+        raise Exception('Invalid Credit Card number. Make sure its only digits with optional spaces.')
     digits = [int(char) for char in number]
     digits = digits[-1::-1]
     doubled = [(digit * 2) if ((i+1) %2 ==0) else digit for (i,digit) in enumerate(digits)]
